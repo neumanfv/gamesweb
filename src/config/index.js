404748d2b7b98fbd40b030ec14b29ea95587ec8a
neuman viana <neumanfv79@gmail.com>
2020-10-27 17:19:37 -0300

    errata gamesweb

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,7 @@ const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
 
   const URL_SERVER = window.location.hostname.includes('localhost')
   ? 'http://localhost:3000'
-  : 'https://filmesweb.vercel.app'; // https://json-filmesweb-heroku.herokuapp.com
+  : 'https://gamesweb.vercel.app'; // https://json-filmesweb-heroku.herokuapp.com
 
 
 export default {
