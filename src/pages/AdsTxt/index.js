81a3c902fca85f6b023d6e81f3b7393dc94374d1
neuman viana <neumanfv79@gmail.com>
2020-12-07 11:30:44 -0300

    mod ads.txt

--- src/pages/AdsTxt/index.js
+++ src/pages/AdsTxt/index.js
@@ -7,8 +7,23 @@ function AdsTxt (){
     return(
 
                <>
-                # Famobi <br/>
-                google.com, pub-2504503821144226, RESELLER, f08c47fec0942fa0
+                    #Softgames
+                    google.com, pub-15426-15624, RESELLER, f08c47fec0942fa0
+
+
+                    # GP (gamepix)
+
+                    google.com, pub-5285623249468085, RESELLER, f08c47fec0942fa0
+
+                    google.com, pub-8435989769185680, RESELLER, f08c47fec0942fa0
+
+                    google.com, pub-2365506394227806, RESELLER, f08c47fec0942fa0
+
+                    rhythmone.com, 1947792888, RESELLER, a670c89d4a324e47
+
+                    rhythmone.com, 1117218481, RESELLER, a670c89d4a324e47
+
+                    rhythmone.com, 3372001037, RESELLER, a670c89d4a324e47
                
                </>
 
